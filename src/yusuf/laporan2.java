e9e797fb7612eb2678998bb56faeef7926688c83
yusufpunde <60200112090@uin-alauddin.ac.id>
2015-05-04 12:48:15 +0800

    laporan2

---
+++ src/yusuf/laporan2.java
@@ -0,0 +1,57 @@
+/**
+ * 
+ */
+package yusuf;
+
+/**
+ * @author Fajri
+ *
+ */
+public class laporan2 {
+	static boolean bool;
+	static byte by;
+	static float f;
+	static String \u0073\u0074\u0072 = "Praktikum nilai default dan Unicode Nita";
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		/**
+		 * @param args
+		 */
+			// TODO Auto-generated method stub
+					if (str == null) {		
+						System.out.println("nilai default boolean " + bool);	
+						System.out.println("nilai default Byte " + by);
+						System.out.println("nilai default Float " + f);
+						System.out.println(str);
+						
+					}
+					else {
+						Boolean refBoolean = new Boolean(true);
+						boolean bool = refBoolean.booleanValue();		
+						System.out.println("bool " + bool);
+						
+						Byte refByte = new Byte((byte) 123);
+						byte b = refByte.byteValue();		
+						System.out.println("Byte " + b);
+						
+						float refFloat = new Float((float) 12.3F);
+						System.out.println("Float " + f);
+						
+						System.out.println(str);
+						
+					}		
+				}
+			
+
+		
+
+
+
+
+	}
+
+
